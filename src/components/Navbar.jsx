c95f185818c1c6457f6d293e4c54d2ae1edfee9a
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-07 21:54:11 +0100

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+       <nav>
+           <h1>
+               <ul>
+                   <li>Home</li>
+                   <li>About</li>
+                   <li>Contact</li>
+               </ul>
+           </h1>
+       </nav>
+    )
+}
+
