0ba9f14bd5c08565ae5d53177721dec4da61efba
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-07 21:53:56 +0100

    Add BookList component

---
+++ src/components/BookList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const BookList = () => {
+  return (
+    <div className="book-list">
+      <li>the way of kings</li>
+      <li>the name of the wind</li>
+      <li>the final empire</li>
+    </div>
+  );
+};
