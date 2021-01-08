57efa383862ffbf4a7e4b0a81d7d7e50298957ac
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-08 17:19:30 +0100

    add logic to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useThemeState} from '../contexts/ThemeContext'
+
 
 export const Navbar = () => {
+
+   
+    const {isLightTheme, light, dark}=useThemeState();
+    const theme = isLightTheme ? light : dark
     return (
-       <nav>
-           <h1>
+       <nav style= {{background:theme.ui, color: theme.syntax}}>
+           <h1>Context App</h1>
                <ul>
                    <li>Home</li>
                    <li>About</li>
                    <li>Contact</li>
                </ul>
-           </h1>
        </nav>
     )
 }
