e48370f181ed8dee100f805b454f2e9d946115af
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-08 17:19:10 +0100

    add logic to BookList

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useThemeState} from '../contexts/ThemeContext'
 
 export const BookList = () => {
+  const contextState = useThemeState()
+    console.log(contextState)
   return (
     <div className="book-list">
       <li>the way of kings</li>
