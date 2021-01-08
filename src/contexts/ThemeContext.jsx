86d31c901995aa9a11b66fa22081589a42d714c0
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-08 17:19:52 +0100

    Add logic to context

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
 
+const ThemeStateContext = React.createContext();
+const ThemeDispatchContext = React.createContext();
 
-const ThemeStateContext =React.createContext();
-const ThemeDispatchContext= React.createContext();
-
-function ThemeProvider () {
-    state = {
-        isLightTheme: true,
-        light: {syntax: '#555', ui: '#ddd', bg: "#eee"},
-        dark: {syntax: '#ddd', ui: '#333', bg: "#555"}
-
-    }
-    return(<ThemeStateContext.Provider>
-        
-        
-        </ThemeStateContext.Provider>
-        );
+function ThemeContextProvider({children}) {
+  const state = {
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+  };
+  return (
+    <ThemeStateContext.Provider value={{ ...state }}>
+      {children}
+    </ThemeStateContext.Provider>
+  );
 }
 
+function useThemeState() {
+    const context = React.useContext(ThemeStateContext);
+    if (context === undefined) {
+      throw new Error('useTodoState must be used within a TodoProvider');
+    }
+    return context;
+  }
 
-
-export { ThemeProvider, ThemeStateContext, ThemeDispatchContext };
+export { ThemeContextProvider, ThemeStateContext, ThemeDispatchContext, useThemeState };
